33f17956e5f29f33b06d48999230f3729dab0514
GreckNavarro <130480169+GreckNavarro@users.noreply.github.com>
2024-03-25 23:41:23 -0500

    Comportamientos

---
+++ Assets/Scripts/Comportamientos.cs
@@ -0,0 +1,188 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comportamientos : MonoBehaviour
+{
+    public float radio;
+    public float maxSpeed;
+    public float Speed;
+    public Transform Target;
+    public float radioWander;
+    public float rateWander;
+    public float frameRateWander = 0;
+    public float obstacleDetectionDistance;
+    public float avoidanceRotationSpeed;
+
+    public void Seek(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        transform.position += direction * Time.deltaTime * maxSpeed;
+    }
+
+    public void Flee(Vector3 target)
+    {
+        Vector3 direction = (transform.position - target).normalized;
+        transform.position += direction * Time.deltaTime * maxSpeed;
+    }
+
+    public void Evade(Transform targetTransform, float predictionTime)
+    {
+        Vector3 targetPos = targetTransform.position + (targetTransform.forward * predictionTime);
+        Flee(targetPos);
+    }
+
+    public void Arrive(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position);
+        float distance = direction.magnitude;
+
+        if(distance < radio)
+        {
+            Speed = Mathf.Clamp(distance/radio,0,radio);
+        }
+        else
+        {
+            Speed = Mathf.Lerp(Speed, maxSpeed, Time.deltaTime * 5);
+        }
+
+        transform.position += direction * Time.deltaTime * Speed;
+
+    }
+
+    public void Wander()
+    {
+        Vector3 rnd = Vector3.zero;
+        Vector3 target = Vector3.zero;
+
+        if(frameRateWander > rateWander)
+        {
+            rnd = Random.insideUnitSphere * radioWander;
+            rnd.y = 0;
+            target = transform.position + transform.forward + rnd;
+            frameRateWander = 0;
+            transform.rotation = Quaternion.LookRotation((target - transform.position).normalized);
+        }
+        frameRateWander += Time.deltaTime;
+        transform.position += transform.forward * Time.deltaTime * Speed;
+    }
+
+    public void Pursuit(Transform targetTransform, float predictionTime)
+    {
+        Vector3 targetPos = targetTransform.position + (targetTransform.forward * predictionTime);
+        Seek(targetPos);
+    }
+
+    public void ObstacleAvoidance()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward,out hit, obstacleDetectionDistance))
+        {
+            Vector3 avoidanceDirection = Vector3.Cross(hit.normal, Vector3.up);
+            Debug.Log(avoidanceDirection);
+            transform.Rotate(avoidanceDirection * avoidanceRotationSpeed * Time.deltaTime);
+            Debug.DrawRay(transform.position, transform.forward * obstacleDetectionDistance, Color.red);
+
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.forward * obstacleDetectionDistance, Color.green);
+
+            transform.position += transform.forward * Time.deltaTime * maxSpeed;
+        }
+
+    }
+
+    public void Separation(List<Transform> agents, float separationDistance)
+    {
+        Vector3 separationForce = Vector3.zero;
+
+        foreach(Transform agent in agents)
+        {
+            float distance = Vector3.Distance(transform.position, agent.position);
+
+            if(distance > 0 && distance < separationDistance)
+            {
+                Vector3 direction = transform.position - agent.position;
+                direction.Normalize();
+                separationForce += direction / distance;
+            }
+        }
+
+        transform.position += separationForce;
+    }
+
+    public Vector3 Cohesion(List<Transform> agents)
+    {
+        Vector3 cohesionCenter = Vector3.zero;
+
+        foreach(Transform agent in agents)
+        {
+            cohesionCenter += agent.position;
+        }
+
+        cohesionCenter /= agents.Count;
+        return cohesionCenter - transform.position;
+    }
+
+
+    public Vector3 Alignment(List<Transform> agents)
+    {
+        Vector3 averageDirection = Vector3.zero;
+
+        foreach (Transform agent in agents)
+        {
+            averageDirection += agent.forward;
+        }
+
+        averageDirection /= agents.Count;
+
+        return averageDirection.normalized;
+    }
+
+    public void PathFollowing(List<Vector3> path, float arrivalRadius)
+    {
+        if(path.Count == 0)
+        {
+            Debug.LogWarning("No hay camino definido para seguir. ");
+            return;
+        }
+
+        Vector3 target = path[0];
+        if(Vector3.Distance(transform.position, target) < arrivalRadius)
+        {
+            path.RemoveAt(0);
+        }
+        if(path.Count > 0)
+        {
+            Seek(target);
+        }
+        else
+        {
+            Arrive(target);
+        }
+
+    }
+
+    private void Update()
+    {
+                
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
